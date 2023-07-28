3e3d8f32f6e74f9a164ebce4edf7b0f8836f6955
adam <adammarey51@gmail.com>
2023-07-27 23:04:14 -0700

    changed randomNum to static - modified numbers class

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,19 +1,19 @@
 import java.util.Random;
 
 public class Numbers {
-    private int randomNum;
+    private static int randomNum;
 
-    public int getRandomNum() {
+    public static int getRandomNum() {
         return randomNum;
     }
 
-    public void setRandomNum(int randomNum) {
-        this.randomNum = randomNum;
+    public void setRandomNum(int number) {
+        randomNum = number;
     }
 
-    public void generateNumber() {
+    public static void generateNumber() {
         Random random = new Random();
-        this.randomNum = random.nextInt(101);
+        randomNum = random.nextInt(101);
 
         // ADAM: for testing purposes
         // System.out.println("Generated random number for testing: " + this.randomNum); // Print the random number
@@ -21,11 +21,11 @@ public class Numbers {
 
     //ADAM:  boolean logic
     public boolean compareNumber(int guess) {
-        if (guess == this.randomNum) {
+        if (guess == randomNum) {
             System.out.println("Congratulations, you guessed the number!");
             return true;
         }
-        else if (guess > this.randomNum) {
+        else if (guess > randomNum) {
             System.out.println("I'm sorry. That guess was too high.");
             return false;
         }
