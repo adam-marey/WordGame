2a1ca3d115c103903beb3a11c5298bbd300ec749
adam <adammarey51@gmail.com>
2023-07-27 23:06:28 -0700

    created person child class - hosts

---
+++ src/Hosts.java
@@ -0,0 +1,9 @@
+public class Hosts extends Person {
+    public Hosts(String firstName) {
+        super(firstName);
+    }
+
+    public void randomizeNum() {
+        Numbers.generateNumber();
+    }
+}
