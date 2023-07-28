68dcdef7d2ac63a18954f6776a742ad93d2954b6
adam <adammarey51@gmail.com>
2023-07-27 22:54:41 -0700

    created Players class

---
+++ src/Players.java
@@ -0,0 +1,21 @@
+public class Players extends Person {
+    private int money;
+
+    public Players(String firstName) {
+        super(firstName);
+        this.money = 1000;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return super.getFirstName() + " has $" + money;
+    }
+}
