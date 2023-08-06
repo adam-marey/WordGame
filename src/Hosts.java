4e0481ce7588547f1b364d88efafb5eaced1086c
adam <adammarey51@gmail.com>
2023-08-05 22:10:34 -0700

    updated host class

--- src/Hosts.java
+++ src/Hosts.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Hosts extends Person {
     public Hosts(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
-    public void randomizeNum() {
-        Numbers.generateNumber();
-//        System.out.println("Generated random number for testing: " + Numbers.getRandomNum());
+    //adam:  replaced with randomizeNum() method
+    public void setNewGamePhrase() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a phrase for players to guess: ");
+        String phrase = scanner.nextLine();
+        Phrases.setGamePhrase(phrase);
     }
 }
